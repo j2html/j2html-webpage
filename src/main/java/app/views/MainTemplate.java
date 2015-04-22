a433c020e57ec9137aa944502176017fb297c3d3
tipsy <davidaase@hotmail.com>
2015-04-22 22:30:50 +0200

    update j2html

--- src/main/java/app/views/MainTemplate.java
+++ src/main/java/app/views/MainTemplate.java
@@ -10,23 +10,23 @@ public class MainTemplate {
     public static String render(String brand, String title, Tag... contentNodes) {
         return document().render() +
                 html().with(
-                head().with(
-                        title().withText(title),
-                        mainCSS()
-                ),
-                body().with(
-                        div().withId("header").with(
-                                h1().withText(brand)
+                        head().with(
+                                title().withText(title),
+                                mainCSS()
                         ),
-                        div().withId("main").with(
-                                div().with(
-                                        contentNodes //content from other template
-                                )
-                        ),
-                        div().withId("footer").withText("Footing around"),
-                        script_jQuery()
-                )
-        ).render();
+                        body().with(
+                                div().withId("header").with(
+                                        h1().withText(brand)
+                                ),
+                                div().withId("main").with(
+                                        div().with(
+                                                contentNodes //content from other template
+                                        )
+                                ),
+                                div().withId("footer").withText("Footing around"),
+                                script_jQuery()
+                        )
+                ).render();
     }
 
 }
