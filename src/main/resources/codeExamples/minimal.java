ef980c21c032919be95deed4fd3d4fc0d5717323
David <davidaase@hotmail.com>
2017-12-31 12:29:35 +0100

    update hello world

--- src/main/resources/codeExamples/minimal.java
+++ src/main/resources/codeExamples/minimal.java
@@ -3,8 +3,8 @@ import static j2html.TagCreator.*;
 public class Main {
     public static void main(String[] args) {
         body(
-            h1("Heading!").withClass("example"),
-            img().withSrc("img/hello.png")
+            h1("Hello, World!"),
+            img().withSrc("/img/hello.png")
         ).render();
     }
 }
