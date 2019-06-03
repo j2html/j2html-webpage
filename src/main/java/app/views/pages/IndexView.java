0a79cd36f84d20af38db652d54e09a06581d91cb
Matthias Braun <mb720@users.noreply.github.com>
2019-06-03 21:51:44 +0200

    Fix typos in IndexView.java

--- src/main/java/app/views/pages/IndexView.java
+++ src/main/java/app/views/pages/IndexView.java
@@ -8,8 +8,8 @@ import static j2html.TagCreator.*;
 public class IndexView {
     public static String render() {
         return MainView.render(
-            "Fast and fluent Java HTML5 builder",
-            "Fast and fluent Java HTML5 builder",
+            "Fast and fluent Java HTML 5 builder",
+            "Fast and fluent Java HTML 5 builder",
             section(attrs("#index"),
                     h2("Getting started with j2html"),
                     p("Import TagCreator to get started. j2html's syntax is fluent and closely matched to HTML:"),
@@ -25,8 +25,8 @@ public class IndexView {
                     h2("Intended use"),
                     h3("Consider using j2html if:"),
                     ul(
-                        li("You love type safety. You love catching errors at compile time in stead of waiting for some poor users to notice that something is wrong"),
-                        li("You like to dynamically re-use your view-code"),
+                        li("You love type safety. You love catching errors at compile time instead of waiting for some poor user to notice that something is wrong"),
+                        li("You like to dynamically reuse your view code"),
                         li(
                             text("You think template engines are too slow. This index page was rendered 100 000 times in less than a second on an i5-4670. That's about a thousand times faster than Apache '"),
                             a("Velocity").withHref("http://velocity.apache.org/").withTarget("_blank"),
@@ -38,7 +38,7 @@ public class IndexView {
                     ul(
                         li("You don't know Java and HTML well"),
                         li("You're creating a classic \"website\" that has a lot of static HTML (if it's all generated then it's fine)"),
-                        li("Your application has a lot of text and you don't use language files / a database (string-concatenation would get very annoying)"),
+                        li("Your application has a lot of text and you don't use language files / a database (string concatenation would get very annoying)"),
                         li("You use a CSS framework which relies on a lot of copy pasting HTML from docs. You'll have to translate these snippets to j2html")
                     ),
 
@@ -46,7 +46,7 @@ public class IndexView {
                     p(
                         "First: j2html is a Java HTML builder. It's not a template engine, and it doesn't want to compete with template engines. "
                             + "We were looking for a good way to create HTML for a complex login solution which had many different forms "
-                            + "(with different configurations, depending on user state and user actions, etc), but very little actual HTML per page. "
+                            + "(with different configurations, depending on user state and user actions, etc.), but very little actual HTML per page. "
                             + "The result was j2html. We decided to release the Java HTML builder we made, since it seems better "
                             + "than all the other Java HTML builders we found while researching the subject. Hopefully someone will find it useful!"
                     )
@@ -54,5 +54,3 @@ public class IndexView {
         );
     }
 }
-
-
